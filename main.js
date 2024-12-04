a7fa0cff18a54956d9bca53b8333ce7b0de35ffc
Louis <178508612+L-Brookling@users.noreply.github.com>
2024-12-04 08:56:34 +0000

    Tasks 1 -> 5 complete

---
+++ main.js
@@ -0,0 +1,54 @@
+// //Task 1: Part 1
+// const favoriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+
+// //Task 1: Part 2
+// const pyramidScheme = ["1", "2", "4", "8", "16", "32", "64"];
+
+// //Task 1: Part 3
+// const friends = [["Chris", "Special skill is changing lives"],["Lizzie", "Special skill is being friendly"],["Ben", "Special skill is code rapping"]]//etc............
+
+//Task 2
+// let options = [
+//     "Jan",
+//     "Feb",
+//     "Mar",
+//     "Apr",
+//     "May",
+//     "Jun",
+//     "Jul",
+//     "Aug",
+//     "Sep",
+//     "Oct",
+//     "Nov",
+//     "Dec",
+//   ];
+  
+// console.log(options[0])
+// console.log(options[2])
+// console.log(options[11])
+
+//Task 3
+// let englishNumbers = ["one", "two", "three", "four", "five"];
+
+// let i = 0
+// while (i < englishNumbers.length) {
+//     console.log(englishNumbers[i]);
+//     i++;
+// };
+//options.forEach((option, i) => console.log(option));
+
+//Task 4
+//for (let j = 0; j < englishNumbers.length; j++) {
+//     console.log(englishNumbers[j]);
+// };
+
+//Task 5
+// let words = ["ben", "ha", "spla", "fa", "ca", "dra"];
+    
+// for (let ii = 0; ii < words.length; ii++) {
+//     console.log((words[ii] + "t"));
+// };
+
+//👉 Use a for loop to iterate over the words array
+//and add the letter t to the end of each word
+//("ben" becomes "bent", "ha" becomes "hat", etc.).
